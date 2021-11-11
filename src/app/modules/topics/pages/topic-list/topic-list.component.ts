57e20af4a80cf89e2890134191b0686274fc280f
Laetitia <laetcrul@gmail.com>
2021-11-11 18:19:22 +0100

    check if registered to topic

--- src/app/modules/topics/pages/topic-list/topic-list.component.ts
+++ src/app/modules/topics/pages/topic-list/topic-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Topic } from 'src/app/models/topic.model';
+import { AuthService } from 'src/app/services/auth.service';
 import { TopicService } from 'src/app/services/topic.service';
+import { User } from "../../../../models/user.model";
 
 @Component({
   selector: 'app-topic-list',
@@ -10,11 +12,16 @@ import { TopicService } from 'src/app/services/topic.service';
 export class TopicListComponent implements OnInit {
   topicList: Topic[] = [];
 
-  constructor(private topicService: TopicService) {
+  constructor(private topicService: TopicService, private authService: AuthService) {
     this.topicService.getAll().subscribe((topics: Topic[]) => this.topicList = topics);
    }
 
   ngOnInit(): void {
   }
 
+  public isSubscribedToTopic(topic: Topic) : boolean{
+    const user: User = <User>  JSON.parse(sessionStorage.getItem('user') || "");
+
+    return topic.subscribedUsers.find((found) => found.id === user.id) != undefined;
+  }
 }
