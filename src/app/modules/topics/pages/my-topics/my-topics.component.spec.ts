e14e2ecbfe2d28856389bfa7c5d9ea2f3b887266
laetcrul <laetcrul@gmail.com>
2021-11-16 12:15:28 +0100

    unversioned files - lists working one to go!

---
+++ src/app/modules/topics/pages/my-topics/my-topics.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyTopicsComponent } from './my-topics.component';
+
+describe('MyTopicsComponent', () => {
+  let component: MyTopicsComponent;
+  let fixture: ComponentFixture<MyTopicsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MyTopicsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyTopicsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
