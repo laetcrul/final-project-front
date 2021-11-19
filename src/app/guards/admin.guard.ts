1c09cfe8bee4f0c876bb94cbc04fab6504c25287
Laetitia <laetcrul@gmail.com>
2021-11-19 15:11:09 +0100

    admin guard file

---
+++ src/app/guards/admin.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {User} from "../models/user.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if(sessionStorage.getItem('user')){
+      const user = <User>  JSON.parse(sessionStorage.getItem('user') || "");
+      return user.roles.find(role => role.label == "ROLE_MANAGE_USERS") != undefined;
+    }
+    return false;
+  }
+}
